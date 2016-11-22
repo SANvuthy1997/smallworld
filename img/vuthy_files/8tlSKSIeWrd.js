99b0085b7500797ff424ba70d8619397ce8fc280
user0 <kimsalsan007@gmail.com>
2016-11-22 15:41:41 +0700

    new

--- img/vuthy_files/8tlSKSIeWrd.js
+++
@@ -1,3 +0,0 @@
-if (self.CavalryLogger) { CavalryLogger.start_js(["XZTrx"]); }
-
-__d("XMessagingOmniMPrefController",["XController"],(function a(b,c,d,e,f,g){c.__markCompiled&&c.__markCompiled();f.exports=c("XController").create("\/messaging\/get_omnim_prefs\/",{});}),null);
